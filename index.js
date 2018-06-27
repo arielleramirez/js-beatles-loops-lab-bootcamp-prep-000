457016f2ff9efdc6f9815f8db4a01537c12796cd
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-27 23:19:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var emptyArray =[];
-  
-  for (var i=0; i < musicians.length; i++) {
-    emptyArray.push(musicians[i] + " plays " + instruments[i]);
-   }
-   return emptyArray;
-}
-
-function johnLennonFacts(){
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-var newFacts = [];
-var i = 0;
-
-while (i < facts.length) {
-      newFacts.push(facts[i] +"!!!");
-        
-    }
-    return newFacts;
-
+  for (var i = 0; i < musician.length; i++){
+    return emptyArray
+  }
+  }
 }
